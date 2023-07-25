00ec9ad668640e1cbe5ad681dc69333b05d5b73d
tantalumDesu <127229020+tantalumDesu@users.noreply.github.com>
2023-07-25 22:40:47 +0100

    Update Dice roller.py
    Now accepts "d20" and such, where the number of dice is implied to be 1.

--- Dice roller.py
+++ Dice roller.py
@@ -3,7 +3,7 @@ import re
 
 def roll_dice(dice_expression):
     # Define a regular expression pattern to match dice expressions with optional negative bonus: 'NdM(+/-)B'
-    pattern = re.compile(r'(\d+)d(\d+)([+-]\d+)?')
+    pattern = re.compile(r'(\d+)?d(\d+)([+-]\d+)?')
     # Remove all whitespace
     dice_expression_clean=re.sub(r"\s+", "", dice_expression)
     # Attempt to match the regular expression to the given dice_expression
@@ -11,14 +11,21 @@ def roll_dice(dice_expression):
     
     if match:
         # Extract the individual components of the dice expression
-        num_dice = int(match.group(1))  # Number of dice to roll (N)
+        num_dice_str = match.group(1)
+        num_dice = int(num_dice_str) if num_dice_str else 1  # Default to 1 if not specified
         dice_faces = int(match.group(2))  # Number of faces on each die (M)
         # Check if there is a bonus specified; if not, default to 0        
         bonus_str = match.group(3)
         bonus = int(bonus_str) if bonus_str else 0
         
         # Roll the dice and calculate the total sum
-        total = sum(randint(1, dice_faces) for _ in range(num_dice)) + bonus
+        roll_total=0
+        for _ in range(num_dice):
+            roll=randint(1, dice_faces)
+            print(roll)
+            roll_total+=roll
+        total=roll_total+bonus
+       # total = sum(randint(1, dice_faces) for _ in range(num_dice)) + bonus
         
         # Return the total sum as the result of the dice roll
         return total
@@ -30,5 +37,5 @@ while True:
     try:
         dice_expression=input("Input dice and modifier in the format NdM(+/-)B: ").lower()
         total=roll_dice(dice_expression)
-        print(total)
+        print(f"Total: {total}")
     except (ValueError): print(f"Invalid dice expression: {dice_expression}\nTry expressions like {randint(1, 20)}d{choice([4,6,8,10,12,20])}{choice(['+','-'])}{randint(1,20)}")
